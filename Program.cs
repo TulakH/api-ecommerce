2527bf0c9184c69499af89472d4c413091cd9eb5
omarb <omar.bahjaoui@outlook.fr>
2026-03-02 17:22:54 +0100

    dbcontext conf

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<PostgreDbContext>(opt => 
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
